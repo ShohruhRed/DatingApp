4da657070b89d1303b5720722f66a8b8ba33a65c
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-09-23 16:25:20 +0300

    Added ConnectionString to SqlLiteDB and initial migrations

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string UserName { get; set; }
 
-        public int MyProperty { get; set; }
+        
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,20 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
