5c3186d231afb45fc3ac1d414c10209559d858a9
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-09-23 17:39:21 +0300

    Add UserController

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUser()
+        {
+            var users = _dataContext.Users.ToList();
+
+            return users;
+        }
+
+        // api/users/2
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _dataContext.Users.Find(id);
+            
+        }
+
+
+    }
+}
