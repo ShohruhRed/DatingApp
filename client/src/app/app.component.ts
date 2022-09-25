3011d6e5683fd27eb3b6b0ed135fe892d0d7a7e8
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-09-26 01:37:20 +0300

    implemented login in nav.components TS

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'The Dating app ';
+  title = 'The Dating app';
   users: any;
 
   constructor(private http: HttpClient){}
