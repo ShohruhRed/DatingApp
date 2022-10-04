3839a9bbb9bf35a28c0d3b9ff124071023229682
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-10-04 23:49:15 +0300

    added a toast service for notifications

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -14,7 +15,8 @@ export class NavComponent implements OnInit {
   currentUser$: Observable<User>;
 
 
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, private router: Router,
+     private toastr: ToastrService ) { }
 
   ngOnInit(): void {
 
@@ -24,7 +26,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: (response) => { console.log(response) , this.router.navigateByUrl('/members') },
 
-      error: error => console.log(error),
+      error: (error) => {console.log(error), this.toastr.error(error.error)}
 
 
 
