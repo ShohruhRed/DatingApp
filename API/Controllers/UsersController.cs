64c2bad14e857460e4d9b493300d5380fd6251ab
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-10-23 23:14:19 +0300

    added UpdateUser(PUT)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,12 +6,13 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    [Authorize]
+    //[Authorize]
     public class UsersController : BaseApiController
     {
         
@@ -25,15 +26,13 @@ namespace API.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet]
-        
+        [HttpGet]        
         public async Task< ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
             var users = await _userRepository.GetMembersAsync();
 
 
-            return Ok(users);
-           
+            return Ok(users);           
         }
 
         // api/users/2
@@ -44,6 +43,23 @@ namespace API.Controllers
             return await _userRepository.GetMemberAsync(username);
             
         }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
+
+        
 
 
     }
