7bb17039719f3fd9e134a4a6ac12f07ce94172ee
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-10-29 20:42:31 +0300

    imlemented setting the main photo(Client) and delete photos(API)

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -16,7 +16,7 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    if(this.members.length > 0) return of(this.members);
+    if (this.members.length > 0) return of(this.members);
 
     return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
       map(members => {
@@ -26,19 +26,24 @@ export class MembersService {
     )
   }
 
-  getMember(username: string){
+  getMember(username: string) {
     const member = this.members.find(x => x.username === username);
 
-    if(member !== undefined) return of(member);
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
- 
- updateMember(member: Member) {
-  return this.http.put(this.baseUrl + 'users', member).pipe(
-    map(() => {
-      const index = this.members.indexOf(member);
-      this.members[index] = member;
-    })
-  );  
- }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
 }
