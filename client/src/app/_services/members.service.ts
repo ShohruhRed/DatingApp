5bd21365fb7616f998aeaed97c1d355fad5a2234
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-12-30 22:25:09 +0300

    Remembering the filters for a user in the service

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, of, pipe } from 'rxjs';
+import { map, of, pipe, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 import { PaginatedResult } from '../_models/pagination';
+import { User } from '../_models/user';
 import { UserParams } from '../_models/userParams';
+import { AccountService } from './account.service';
 
 
 
@@ -15,8 +17,28 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
   memberCache = new Map();
+  user: User;
+  userParams: UserParams;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user;
+      this.userParams = new UserParams(user);
+    })
+   }
+
+   getUserParams(){
+    return this.userParams;
+   }
+
+   setUserParams(params: UserParams){
+    this.userParams = params
+   }
+
+   resetUserParams(){
+    this.userParams = new UserParams(this.user);
+    return this.userParams;
+   }
 
   getMembers(userParams: UserParams) {
     var response = this.memberCache.get(Object.values(userParams).join('-'));
