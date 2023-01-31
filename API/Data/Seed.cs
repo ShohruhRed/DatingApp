667d6d088c9f561ade43e36d2263c8f9e49a2023
Shohruh Abduvohidov <shohruhred@inbox.ru>
2023-01-31 22:52:31 +0300

    added roles to the app

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -9,16 +9,26 @@ namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager)
+        public static async Task SeedUsers(UserManager<AppUser> userManager, 
+            RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync()) return;
 
             var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
-            IQueryable<AppUser> appUsers;
+            if (users == null) return;
 
-            IdentityResult result; 
-            
+            var roles = new List<AppRole>
+            {
+                new AppRole {Name = "Member"},
+                new AppRole {Name = "Admin"},
+                new AppRole {Name = "Moderator"}
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
 
             foreach (var user in users)
             {
@@ -26,9 +36,17 @@ namespace API.Data
 
                 user.PasswordHash = userManager.PasswordHasher.HashPassword(user, "seedPassword");
 
-                await userManager.CreateAsync(user);                           
-                    
-            }           
+                await userManager.CreateAsync(user);
+                await userManager.AddToRoleAsync(user, "Member");                    
+            }
+
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
+
+            await userManager.CreateAsync(admin, "Pa$$w0rd");
+            await userManager.AddToRolesAsync(admin, new[] { "Admin", "Moderator" });
         }
     }
 }
