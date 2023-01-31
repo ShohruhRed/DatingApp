bf986ce6ff854e6e3d65cf34a02d89c838e3e600
Shohruh Abduvohidov <shohruhred@inbox.ru>
2023-01-31 23:34:18 +0300

    Added policy based authorization

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -31,7 +31,7 @@ namespace API.Controllers
             _photoService = photoService;
         }
 
-        [Authorize(Roles = "Admin")]
+        
         [HttpGet]        
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
@@ -51,7 +51,7 @@ namespace API.Controllers
 
 
         // api/users/2
-        [Authorize(Roles = "Member")]
+        
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
